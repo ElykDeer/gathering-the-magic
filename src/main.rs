bf89730fde525b0ae1cd470e72f75bb3af0dfa4f
KyleMiles <krm504@nyu.edu>
2024-04-29 00:55:05 -0400

    Add initialization detection

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,20 @@ use warp::{
 
 #[tokio::main]
 async fn main() {
+    if !(std::path::Path::new("./scryfall.db").exists()
+        && std::path::Path::new("./images/").exists())
+    {
+        println!("Scryfall data does not exist. Do you want to download it? This will take 3-4 hours, 4.2GB of disk, and is required only once. (y/N)");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        if input.trim().eq_ignore_ascii_case("y") {
+            scryers::download_all_cards();
+        }
+    }
+
     let static_files = warp::get().and(warp::fs::file("./index.html"));
     let image_route = warp::path("images").and(warp::fs::dir("../gathering_the_magic/images/"));
 
